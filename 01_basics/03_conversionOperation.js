25a32fd59ed845a6e325e6a487b81d2230dceec0
Rishab Shukla <112012613+Rishab06@users.noreply.github.com>
2025-02-21 13:57:30 +0000

    Conversion of datatype operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score="rishab"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33"=> 33
+//"33abc" => NaN
+//true=>1; flase=>0;
+
+
+
+let isLoggedIn="hrgufh"
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false;
+//"" => false;
+//"Risahb" => true;
+
+
+
+let someNumber = 33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
