da06caaba3b683688535c1ea3d09056287b7237d
Rishab Shukla <112012613+Rishab06@users.noreply.github.com>
2025-02-21 07:11:30 +0000

    let const done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId=14453;
+let accountEmail="rs747@gmail.com";
+var accountPassword="12345";
+accountCity="Jaipur";
+let accountState;
+
+// accountId=2; not allowed
+accountEmail='gye@gmail.com';
+accountPassword="456486";
+accountCity="ludhiana";
+console.log(accountId);
+console.table([accountId,accountPassword,accountCity,accountState]);
+console.log(accountState);
+
+
+/* Prefer not to use var
+because of issue in block scope and functional scope*/ 
