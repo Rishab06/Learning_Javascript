5f143e5f52b8e9a9a91825e0c123a7d0e3b055bf
Rishab Shukla <112012613+Rishab06@users.noreply.github.com>
2025-02-21 08:27:21 +0000

    Datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all Js code as newer version.
+
+//alert(3+3)  // We are using nodejs , not browsers.
+
+console.log(3+
+    3); // code readabilty should be high.
+
+
+    console.log("Rishab")
+
+    let name="Rishab Shukla"
+    let age=18
+    let isloggedin=false
+
+    //number => 2 to power 53
+    // bigint
+    // string =>""
+    //boolean => true/false
+    //null => standalone value or null is object. 
+    // undefined => value not assign or undefined itself a datatype
+    //symbol => unique
+
+
+
+    //object
+
+
+    console.log(typeof undefined) //undefined
+    console.log(typeof null) //object
+    
+    
+
