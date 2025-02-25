9354e38cb5563f5d6a9ac6f1a93e51b123196189
Rishab Shukla <112012613+Rishab06@users.noreply.github.com>
2025-02-25 09:09:36 +0000

    String to Number

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,6 +28,58 @@ let stringNumber=String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+// ***************************************Operation**************************************************************
+let value=3
+let negvalue=-value
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2)
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1="Rishab"
+let str2="shukla"
+
+let str3=str1+str2
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log(3+4*5%3);
+
+
+console.log(+true); //In JavaScript, the unary plus operator (+) is used to convert its operand to a number. +true converts the boolean true to the number 1.
+console.log(+"");  //+"" converts an empty string to the number 0.
+//It's a shorthand way to perform type conversion. If the conversion isn't possible (e.g., +"hello"), it will result in NaN (Not a Number).
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
